329aaa420c63b2e7331e2d325715cc89ec57da87
sx9dev <sx-91@outlook.com>
2023-04-14 13:30:40 +0700

    1st

---
+++ index.js
@@ -0,0 +1,61 @@
+#! /bin/node
+
+import express from "express";
+import path from "path";
+import { execaSync as exec } from "execa";
+import { createServer } from "http";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: {
+    origin: "*"
+}});
+
+if (process.platform !== 'linux') throw new Error('Unsupported Platform!');
+
+app.use(express.static(__dirname + '/public'));
+io.on("connection", (socket) => {
+    socket.on('play', () => exec('playerctl', ['play-pause']));
+    socket.on('next', () => exec('playerctl', ['next']));
+    socket.on('prev', () => exec('playerctl', ['previous']));
+    socket.on('shuf', () => exec('playerctl', ['shuffle', 'toggle']));
+    socket.on('loop', () => {
+        let currently;
+        try {
+            currently = exec('playerctl', ['loop']).stdout;
+        } finally {
+            switch (currently) {
+                case 'None':
+                    exec('playerctl', ['loop', 'playlist']);
+                    break;
+                case 'Playlist':
+                    exec('playerctl', ['loop', 'track']);
+                    break;
+                case 'Track':
+                    exec('playerctl', ['loop', 'none']);
+                    break;
+            }
+        }
+    });
+});
+
+setInterval(() => {
+    let output = {};
+    try {
+        output = {
+            title: exec('playerctl', ['metadata', 'xesam:title']).stdout,
+            artist: exec('playerctl', ['metadata', 'xesam:artist']).stdout,
+            status: exec('playerctl', ['status']).stdout,
+            loop: exec('playerctl', ['loop']).stdout,
+        }
+    } finally {
+        io.sockets.emit('update', output);
+    }
+}, 500);
+
+httpServer.listen(7564, () => console.log('http://localhost:7564'));
